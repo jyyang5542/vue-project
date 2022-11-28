bccd4951e79cdb6621ea0cab86f59be1ff68e25a
양정연 <triciayang@iocentre.biz>
2022-11-28 11:08:40 +0900

    -

--- src/App.vue
+++ src/App.vue
@@ -3,50 +3,57 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <ul>
-          <!-- Gallery -->
-          <li>
-            <RouterLink to="/gallery">Gallery</RouterLink>
-            <ul>
-              <li><RouterLink to="/gallery">Original</RouterLink></li>
-              <li><RouterLink to="/gallery">Games</RouterLink></li>
-            </ul>
-          </li>
-          <!-- Screenshot -->
-          <li>
-            <RouterLink to="/screenshot">ScreenShot</RouterLink>
-            <ul>
-              <li><RouterLink to="/screenshot">FFXIV</RouterLink></li>
-            </ul>
-          </li>
-          <!-- B-Tool -->
-          <li>
-            <RouterLink to="/btool">B-Tool</RouterLink>
-            <ul>
-              <li><RouterLink to="/btool">Original</RouterLink></li>
-              <li><RouterLink to="/btool">Games</RouterLink></li>
-              <li><RouterLink to="/btool">Transformers</RouterLink></li>
-              <li><RouterLink to="/btool">Etc</RouterLink></li>
-            </ul>
-          </li>
-          <!-- Etc -->
-          <li>
-            <RouterLink to="/etc">Etc</RouterLink>
-            <ul>
-              <li><RouterLink to="/etc">Links</RouterLink></li>
-            </ul>
-          </li>
-          <!-- Clap -->
-          <li><RouterLink to="/clap">Clap</RouterLink></li>
-        </ul>
-      </nav>
-    </div>
-  </header>
+  <div class="hatiho">
+    <header class="hatiho__header">
+      <div class="wrapper">
+        <h1 class="hatiho__logo"><RouterLink to="/">HatiHo</RouterLink></h1>
+        <nav class="hatiho__gnb">
+          <ul class="hatiho__menu">
+            <!-- Gallery -->
+            <li>
+              <RouterLink to="/gallery">Gallery</RouterLink>
+              <ul class="hatiho__submenu">
+                <li><RouterLink to="/gallery">Original</RouterLink></li>
+                <li><RouterLink to="/gallery">Games</RouterLink></li>
+              </ul>
+            </li>
+            <!-- Screenshot -->
+            <li>
+              <RouterLink to="/screenshot">ScreenShot</RouterLink>
+              <ul class="hatiho__submenu">
+                <li><RouterLink to="/screenshot">FFXIV</RouterLink></li>
+              </ul>
+            </li>
+            <!-- B-Tool -->
+            <li>
+              <RouterLink to="/btool">B-Tool</RouterLink>
+              <ul class="hatiho__submenu">
+                <li><RouterLink to="/btool">Original</RouterLink></li>
+                <li><RouterLink to="/btool">Games</RouterLink></li>
+                <li><RouterLink to="/btool">Transformers</RouterLink></li>
+                <li><RouterLink to="/btool">Etc</RouterLink></li>
+              </ul>
+            </li>
+            <!-- Etc -->
+            <li>
+              <RouterLink to="/etc">Etc</RouterLink>
+              <ul class="hatiho__submenu">
+                <li><RouterLink to="/etc">Links</RouterLink></li>
+              </ul>
+            </li>
+            <!-- Clap -->
+            <li><RouterLink to="/clap">Clap</RouterLink></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
 
-  <RouterView />
+    <section class="hatiho__content">
+      <RouterView />
+    </section>
+
+    <footer class="hatiho__footer">Copyright 2017 ~ 2022 <b>ⓒHatiho</b> All Rights Reserved.</footer>
+  </div>
 </template>
 
 <style lang="scss"></style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/scss/layout.scss";
+import "./assets/scss/pc.scss";
+import "./assets/scss/tablet.scss";
+import "./assets/scss/mobile.scss";
 
 const app = createApp(App);
 
